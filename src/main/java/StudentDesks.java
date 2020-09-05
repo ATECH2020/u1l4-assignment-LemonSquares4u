fafe75212d94b140dc08efa13686ca6266fde101
Miguel Rivera Villareal <miguel.263282@nv.ccsd.net>
2020-09-05 23:11:08 +0000

    Potentially Finished up StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -4,17 +4,28 @@ class StudentDesks {
     public static void main(String[] args) {
 
         //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanboi = new Scanner(System.in);
 
         //Reads ints from user
-        
+         int classOneKids = scanboi.nextInt();
+         int classTwoKids = scanboi.nextInt();
+         int classThreeKids = scanboi.nextInt();
 
+         //a desk holds two kids 
         /*
          *  your code goes here
          */
+         int classOneDesksNeed,classTwoDesksNeed, classThreeDesksNeed, desksNeeded = 0;
 
+         classOneDesksNeed = classOneKids / 2.0;
+         classTwoDesksNeed = classTwoKids / 2.0;  
+         classThreeDesksNeed = classThreeKids / 2.0;  
+
+         desksNeeded = classOneDesksNeed + classTwoDesksNeed + classThreeDesksNeed;
+
+          System.out.print(desksNeeded);
 
         // closing the scanner object
-        scanner.close();
+        scanboi.close();
     }
 }
