1d517d9e53ccc8bad0149cbace9cc792a1ddb20e
Miguel Rivera Villareal <miguel.263282@nv.ccsd.net>
2020-09-05 22:25:34 +0000

    created my variables for AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -4,14 +4,18 @@ class AnalogClock {
     public static void main(String[] args) {
 
         //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanboi = new Scanner(System.in);
 
         //Reads ints from user
-
+        int hourHandDegree = scanboi.nextInt();
 
         /*
          *  your code goes here
          */
+
+         int partialHour, remaindingDegree, remainder, minutes = 0;
+
+         
 
         // closing the scanner object
         scanner.close();
